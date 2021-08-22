5e151a9cce972bd2adda224994db56cfc5175d3d
Uche Okoroafor <ucheokoroafor018@gmail.com>
2021-08-22 12:58:54 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Streamer from './Streamer'
+import Nav from './components/Nav'
+import UserPage from './components/UserPage'
+import { BrowserRouter as Router,Route} from "react-router-dom"
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+<Router>
+<Nav/>
+
+
+
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-    i am missing react
-        </p>nnjj
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,7 +26,13 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+
+<Route  path="/" exact component={Streamer} />
+<Route  path="/users"  component={UserPage} />
+
+</Router>
+
     </div>
   );
 }
