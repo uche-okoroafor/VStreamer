3053e087bc49b7eb1b28fe01337e0dd24de7640b
Uche Okoroafor <ucheokoroafor018@gmail.com>
2021-08-29 13:03:13 +0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const UserAccess = require("./models/userAccess");
 const loginRoute = require('./routes/loginRoute.js')
 const createAccountRoute = require('./routes/createAccountRoute.js')
+const usersRoute =  require('./routes/usersRoute.js')
+const bcrypt = require('bcrypt')
 require('dotenv/config');
 
 const app = express();
@@ -15,7 +17,8 @@ app.use(express.urlencoded({ extended: true }));
 
 mongoose
   .connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then((result) => {app.listen(5000)
+  .then((result) => {
+app.listen(5000)
 console.log("connected to data Base")
 })
   .catch((err) => console.log(err));
@@ -24,4 +27,20 @@ console.log("connected to data Base")
 
 app.use("/login",loginRoute);
 app.use("/create_account",createAccountRoute);
+app.use("/users/",usersRoute);
+
+
+
+
+
+// app.get("/users",(req,res)=>{
+// console.log(req.url,200)
+// res.json({
+// user:["user1","user220000"]
+
+// })
+
+// })
+
+
 
