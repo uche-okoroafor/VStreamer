e2d9aaec0dbfc738dce5d7cea745c8cab4a9a85f
Uche Okoroafor <ucheokoroafor018@gmail.com>
2021-09-10 21:23:53 +0400

    Update usersController.js

--- BackEnd/Controllers/usersController.js
+++ BackEnd/Controllers/usersController.js
@@ -1,7 +1,57 @@
-exports.usersController=(req,res)=>{
-res.json({
-usersList:["user1","user2"]
+const UserAccess = require("../models/userAccess");
+const Users = require("../models/users");
+const bcrypt = require("bcrypt");
 
-})
+exports.uploadVideoController = async (req, res) => {
+console.log(req.body)
+  try {
+    const uploadVideo = await Users.updateOne(
+      { _id:req.params.userId},
+      {
+        $push: {
+          videos: [
+           req.body
+          ],
+        },
+      }
+    );
 
-}
+    res.send(uploadVideo);
+  } catch (err) {
+    res.send(err);
+  }
+};
+
+// app.post("/postupdate/",async (req, res) => {
+//      try{
+
+// const uploadVideo = await  Users.updateOne({_id:"61307544055f04b7c00b2203"},{
+//  $push:{videos:[
+// {
+//     videoSource: "https://youtu.be/1WHPExTeOwg",
+//         videoTitle: "james",
+//         videoId: "gfhhfgjgjgjg",
+//       },
+// ]}}
+// )
+
+// res.send(dateVideo)
+// // const updateVideo = await Users.collection("videos").insertOne(req.body, function(err, res) {
+// //     if (err) throw err;
+// //     console.log("1 document inserted");
+// //    Users.close();
+// //   });
+
+// // const updateVideo = await Users.updateOne(
+// // {_id:req.params.userId },{
+// // $set:{videos:req.body}
+// // }
+
+// // );
+// // res.json(updateVideo);
+//      }catch(err){
+
+// res.send(err)
+// }
+
+// });
