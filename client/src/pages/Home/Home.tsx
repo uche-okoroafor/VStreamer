6d9eabb9a2a0d8c472460d5c8a1ee8577c95208b
Uche Okoroafor <ucheokoroafor018@gmail.com>
2021-10-20 21:25:11 +0400

    Update Home.tsx

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -6,20 +6,16 @@ import { useAuth } from '../../context/useAuthContext';
 export default function Home(): JSX.Element {
   const { loggedInUser } = useAuth();
 
-
   return (
     <Container>
       <Typography variant="h3" align="center">
         welcome {loggedInUser ? loggedInUser.username : 'Guest'}
       </Typography>
       <Paper style={{ minHeight: '80vh', maxWidth: '80%', margin: '0 auto' }}>
-<Box>
-
-<VideosList/>
-
-</Box>
-
-</Paper>
+        <Box justifyContent="center">
+          <VideosList />
+        </Box>
+      </Paper>
     </Container>
   );
 }
