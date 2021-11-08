2c8da64ce6c335d2ef00a9d5a88aaad7865a5360
Uche Okoroafor <ucheokoroafor018@gmail.com>
2021-11-08 22:40:17 +0400

    Update uploadVideoController.js

--- server/controllers/uploadVideoController.js
+++ server/controllers/uploadVideoController.js
@@ -1,19 +1,43 @@
 const User = require('../models/User')
 const fs = require('fs')
+const cloud = require('../config/cloudinaryConfig')
 
 exports.uploadVideoDetailsController = async (req, res) => {
-  const userId = req.user.id
-  const { videoTitle, videoSource, videoId } = req.body
-  if ((!userId, !videoTitle, !videoSource, !videoId)) {
+  const {
+    username,
+    userId,
+    title,
+    source,
+    videoId,
+    videoDescription,
+    videoTags,
+    category
+  } = req.body
+  if ((!userId, !title, !source, !videoId)) {
     return res
       .status(400)
-      .send({ err: 'userId,videoSource,videoTitle or videoId is undefined' })
+      .send({ err: 'userId,source,title or videoId is undefined' })
   }
 
   try {
     const videoUpdateStatus = await User.updateOne(
       { _id: userId },
-      { $push: { videos: [{ videoTitle, videoSource, videoId }] } }
+      {
+        $push: {
+          videos: [
+            {
+              username,
+              userId,
+              title,
+              source,
+              videoId,
+              videoDescription,
+              videoTags,
+              category
+            }
+          ]
+        }
+      }
     )
 
     res.status(200).json(videoUpdateStatus)
@@ -23,64 +47,44 @@ exports.uploadVideoDetailsController = async (req, res) => {
 }
 
 exports.uploadVideoController = async (req, res) => {
-  console.log(req.files)
+  const {
+    username,
+    userId,
+    title,
+    source,
+    videoId,
+    videoDescription,
+    videoTags,
+    category
+  } = req.body
 
-  if (!req.params.videoId) {
-    return res.status(400).json({ meg: 'videoId not defined' })
+  // const userId = req.user.id
+  try {
+    const uploadStatus = await cloud.uploads(req)
+    // if (uploadStatus) {
+    //   const videoUpdateStatus = await User.updateOne(
+    //     { _id: userId },
+    //     {
+    //       $push: {
+    //         videos: [
+    //           {
+    //             username,
+    //             userId,
+    //             title,
+    //             source:uploadStatus.url,
+    //             videoId:uploadStatus.asset_id,
+    //             videoDescription,
+    //             videoTags,
+    //             category
+    //           }
+    //         ]
+    //       }
+    //     }
+    //   )
+    // }
+
+    return res.status(200).send(uploadStatus)
+  } catch (err) {
+    return res.status(500).send('something went wrong')
   }
-  if (req.files === null || !req.files) {
-    return res.status(400).json({ meg: 'no file upload' })
-  }
-  const { file } = req.files
-  file.mv(`./uploads/${file.name}`, err => {
-    if (err) {
-      console.log(err, 5000)
-      return res.status(500).send(err)
-    }
-    fs.rename(
-      `./uploads/${file.name}`,
-      `./uploads/${req.params.videoId}.mp4`,
-      err => {
-        if (err) {
-          console.log(err, 500)
-          return res.status(500).send(err)
-        }
-      }
-    )
-    res.json({ fileName: file.name, filePath: `/uploads/${file.name}` })
-  })
 }
-
-// app.post("/postupdate/",async (req, res) => {
-//      try{
-
-// const uploadVideo = await  Users.updateOne({_id:"61307544055f04b7c00b2203"},{
-//  $push:{videos:[
-// {
-//     videoSource: "https://youtu.be/1WHPExTeOwg",
-//         videoTitle: "james",
-//         videoId: "gfhhfgjgjgjg",
-//       },
-// ]}}
-// )
-
-// res.send(dateVideo)
-// // const updateVideo = await Users.collection("videos").insertOne(req.body, function(err, res) {
-// //     if (err) throw err;
-// //     console.log("1 document inserted");
-// //    Users.close();
-// //   });
-
-// // const updateVideo = await Users.updateOne(
-// // {_id:req.params.userId },{
-// // $set:{videos:req.body}
-// // }
-
-// // );
-// // res.json(updateVideo);
-//      }catch(err){
-
-// res.send(err)
-// }
-
-// });
