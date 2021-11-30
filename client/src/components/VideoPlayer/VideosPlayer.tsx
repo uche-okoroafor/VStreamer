14ff2674e44b89b62a83224da66f1ded569cb4b6
Uche Okoroafor <ucheokoroafor018@gmail.com>
2021-11-30 23:09:28 +0400

    Update VideosPlayer.tsx

--- client/src/components/VideoPlayer/VideosPlayer.tsx
+++ client/src/components/VideoPlayer/VideosPlayer.tsx
@@ -1,10 +1,12 @@
+import { duration } from '@material-ui/core';
 import { Box, CircularProgress } from '@mui/material';
 import { Typography } from '@mui/material';
 import { Component, useEffect, useRef } from 'react';
+import Player from 'react-player';
 
 interface IProps {
   videoSource: string | undefined;
-
+  setVideoDuration: React.Dispatch<string>;
   videoPlayerOptions: {
     width: string;
     height: string;
@@ -14,36 +16,46 @@ interface IProps {
   };
 }
 
-export default function VideoPlayer({ videoSource, videoPlayerOptions }: IProps): JSX.Element {
+export default function VideoPlayer({ videoSource, videoPlayerOptions, setVideoDuration }: IProps): JSX.Element {
   const { width, height, autoPlay, component, classes } = videoPlayerOptions;
 
-  // useEffect(() => {
-  //   if (videoSource) console.log(videoSource);
-  // }, [videoSource]);
+  const handleDuration = (duration: number): void => {
+    const durationInMinutes = duration / 60;
+    setVideoDuration(durationInMinutes.toFixed(2).toString());
+  };
 
   return (
     <>
       {videoSource ? (
-        !videoSource.includes('youtube') ? (
-          <video id="video" autoPlay={autoPlay} width={width} height={height} controls style={{ zIndex: 0 }}>
-            <source src={videoSource} type="video/mp4" />
-            <source src={videoSource} type="video/ogg" />
-            Your browser does not support HTML video.
-          </video>
-        ) : (
-          <iframe
-            width={width}
-            height={height}
-            src={videoSource}
-            title="YouTube video player"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          ></iframe>
-        )
+        <Player
+          playing={autoPlay}
+          controls
+          width={parseInt(width)}
+          onDuration={(duration) => handleDuration(duration)}
+          height={parseInt(height)}
+          style={{ zIndex: 0 }}
+          url={videoSource}
+        />
       ) : (
+        // !videoSource.includes('youtube') ? (
+        //   <video id="videoId" autoPlay={autoPlay} width={width} height={height} controls style={{ zIndex: 0 }}>
+        //     <source src={videoSource} type="video/mp4" />
+        //     <source src={videoSource} type="video/ogg" />
+        //     Your browser does not support HTML video.
+        //   </video>
+        // ) : (
+        //   <iframe
+        //     width={width}
+        //     height={height}
+        //     src={videoSource}
+        //     title="YouTube video player"
+        //     frameBorder="0"
+        //     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        //     allowFullScreen
+        //   ></iframe>
+        // )
         <>
-          <video id="video" autoPlay={autoPlay} width={width} height={height} controls style={{ zIndex: 0 }}>
+          <video id="videoId" autoPlay={autoPlay} width={width} height={height} controls style={{ zIndex: 0 }}>
             <source src={videoSource} type="video/mp4" />
             <source src={videoSource} type="video/ogg" />
             Your browser does not support HTML video.
