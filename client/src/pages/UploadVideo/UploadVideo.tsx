11c8e16b69f5fef310f6cbbc91888a850300c20b
Uche Okoroafor <ucheokoroafor018@gmail.com>
2021-12-01 22:48:09 +0400

    Update UploadVideo.tsx

--- client/src/pages/UploadVideo/UploadVideo.tsx
+++ client/src/pages/UploadVideo/UploadVideo.tsx
@@ -1,14 +1,16 @@
 /* eslint-disable prettier/prettier */
 import { Container, Typography, Button } from '@material-ui/core';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useAuth } from '../../context/useAuthContext';
 import UploadVideoForm from './UploadVideoForm/UploadVideoForm';
-import { uploadVideoDetails, uploadVideoLocally } from '../../helpers/APICalls/uploadVideo';
+import { uploadVideoDetails } from '../../helpers/APICalls/uploadVideo';
 import axios from 'axios';
 import { v4 as uuidv4 } from 'uuid';
 import { useAllVideos } from '../../context/useAllVideosContext';
 
 import { IVideoDetails } from '../../interface/VideoDetails';
+import { useSnackBar } from '../../context/useSnackbarContext';
+import { IViewerResponse } from '../../interface/ApiResponse';
 
 interface IState {
   stringValue: string;
@@ -25,138 +27,100 @@ interface IState {
 export default function UploadVideo(): JSX.Element {
   const { loggedInUser } = useAuth();
   const [file, setFile] = useState<IState['file']>(null);
-  //  const [file, setFile] = useState(null);
-
+  const [videoDuration, setVideoDuration] = useState('');
   const [fileName, setFileName] = useState('Choose a file');
   const [uploadSuccess, setUploadSuccess] = useState<boolean | undefined>(undefined);
   const [uploadProgress, setUploadProgress] = useState<IState['numberValue']>(0);
   const [videoSource, setVideoSource] = useState('');
   const [videoTitle, setVideoTitle] = useState('');
-  const [videoId, setVideoId] = useState<string>('');
   const [isSubmitting, setSubmitting] = useState(false);
   const { handleGetAllVideos } = useAllVideos();
+  const { updateSnackBarMessage } = useSnackBar();
+  const [artist, setArtist] = useState('');
+  const [description, setDescription] = useState('');
+  const [category, setCategory] = useState('');
+
+  console.log(videoDuration, 'videoDuration');
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
     e.preventDefault();
+
+    if (!videoTitle || !description || !category) {
+      return;
+    }
+
     setSubmitting(true);
-    const video_Id = uuidv4();
-    let videoDetails: IVideoDetails | undefined = undefined;
-    setVideoId(video_Id);
     if (loggedInUser) {
-      videoDetails = {
-        username: loggedInUser.username,
-        userId: loggedInUser.id,
+      const videoDetails: IVideoDetails = {
+        username: loggedInUser?.username,
+        userId: loggedInUser?.id,
+        userImage: loggedInUser?.userImage,
         videoTitle,
+        videoId: '',
         videoSource,
-        videoId: video_Id,
-        videoDescription: '',
-        videoTags: '',
-        videoCategory: '',
+        videoDescription: description,
+        artist: artist,
+        videoCategory: category,
+        videoDuration,
+        _id: '',
       };
-    }
-
-    if (file) {
-      const formData: any = new FormData();
-      formData.append('file', file);
-      try {
-        await axios
-          .post(`/upload_video/${videoId}`, formData, {
-            headers: {
-              'content-Type': 'multipart/form-data',
-            },
-            onUploadProgress: (progressEvent) => {
-              setUploadProgress(Math.round((progressEvent.loaded * 100) / progressEvent.total));
-            },
-          })
-          .then(async (response) => {
-            try {
-              await uploadVideoDetails(videoDetails);
-            } catch (err) {
-              console.log(err);
-            }
-          })
-          .catch((err) => console.log(err));
-      } catch (err) {
-        console.log(err, 400);
-      }
-      // handleGetAllVideos()
-    } else {
-      try {
-        console.log(videoDetails, 2001000);
-        const response = await uploadVideoDetails(videoDetails);
-        if (response) {
-          setUploadSuccess(true);
-          handleGetAllVideos();
-        }
-      } catch (err) {
-        if (err) {
-          setUploadSuccess(false);
-
-          console.log(err, 400);
-          setTimeout(() => {
-            setUploadSuccess(undefined);
-          }, 5000);
-        }
+      if (file) {
+        await handleUploadVideoToCloud(videoDetails);
+      } else {
+        await handleUploadVideoDetails(videoDetails);
       }
     }
-
-    // handleUpdataData(user._id, user.userName, true);
     setSubmitting(false);
   };
 
-  // const handleUploadVideoDetails = async (e) => {
-  //   e.preventDefault();
-  //   const videoDetails = {
-  //     title,
-  //     videoSource,
-  //     videoId,
-  //   };
-  //   try {
-  //     const response = axios.post(
-  //       `/upload_video/video_details/${user.userName}/${user._id}`,
-  //       videoDetails
-  //     );
+  const handleUploadVideoToCloud = async (videoDetails: IVideoDetails) => {
+    const formData: any = new FormData();
+    formData.append('video', file);
+    try {
+      const { data } = await axios.post(`/video/upload-video`, formData, {
+        headers: {
+          'content-Type': 'multipart/form-data',
+        },
+        onUploadProgress: (progressEvent) => {
+          setUploadProgress(Math.round((progressEvent.loaded * 100) / progressEvent.total));
+        },
+      });
+      if (data) {
+        videoDetails.videoSource = data.url;
+        console.log(videoDetails, 10101);
+        await handleUploadVideoDetails(videoDetails);
+      } else {
+        updateSnackBarMessage('video not uploaded');
+      }
+    } catch (err) {
+      console.error(err);
+      updateSnackBarMessage('video not uploaded');
+    }
+  };
 
-  //     if (file) {
-  //       handleUploadVideoLocally(videoId);
-  //     }
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
+  const handleUploadVideoDetails = async (videoDetails: IVideoDetails): Promise<void> => {
+    try {
+      const { data } = await uploadVideoDetails(videoDetails);
+      console.log(data, data?.success);
+      if (data?.success) {
+        setUploadSuccess(true);
+        handleGetAllVideos();
+      } else {
+        updateSnackBarMessage('video not uploaded');
+      }
+    } catch (err) {
+      setUploadSuccess(false);
 
-  //   handleUpdataData(user._id, user.userName, true);
-  // };
-
-  // const handleUploadVideoLocally = async (videoId) => {
-  //   const formData = new FormData();
-  //   console.log(user);
-  //   formData.append("file", file);
-  //   try {
-  //     const response = await axios.post(`/upload_video/${videoId}`, formData, {
-  //       headers: {
-  //         "content-Type": "multipart/form-data",
-  //       },
-  //       onUploadProgress: (progressEvent) => {
-  //         setUploadProgress(
-  //           parseInt(
-  //             Math.round(progressEvent.loaded * 100) / progressEvent.total
-  //           )
-  //         );
-  //       },
-  //     });
-
-  //     const { fileName, filePath } = response.data;
-  //   } catch (err) {
-  //     if (err.response.status === 500) {
-  //       console.log("server-problem");
-  //     } else {
-  //       console.log(err.response.status, 5000);
-  //     }
-  //   }
-  // };
+      console.error(err);
+      updateSnackBarMessage('video not uploaded');
+    }
+    setTimeout(() => {
+      setUploadSuccess(undefined);
+    }, 5000);
+  };
 
   return (
-    <Container style={{ backgroundColor: '#f0f0f0', minHeight: '90vh' }}>
+    <Container style={{ minHeight: '90vh' }}>
       <UploadVideoForm
         handleSubmit={handleSubmit}
         videoSource={videoSource}
@@ -168,6 +132,11 @@ export default function UploadVideo(): JSX.Element {
         isSubmitting={isSubmitting}
         uploadProgress={uploadProgress}
         uploadSuccess={uploadSuccess}
+        setVideoDuration={setVideoDuration}
+        setArtist={setArtist}
+        setDescription={setDescription}
+        setCategory={setCategory}
+        category={category}
       />
     </Container>
   );
